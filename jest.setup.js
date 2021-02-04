8ca76f057a2fb4325371881485be4069db681365
Thomsen Cummings <thomsencummings@gmail.com>
2021-02-03 23:22:21 -0500

    remove fetch mock import from jest setup and replace with comment explaining how to globally mock a function

--- jest.setup.js
+++ jest.setup.js
@@ -2,8 +2,9 @@ import React from "react";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-import _fetch from "./__mocks__/fetch";
-
-global.fetch = _fetch;
+// import any mocks here and set them as globals
+// e.g.
+// import _fetch from "./__mocks__/fetch";
+// global.fetch = _fetch;
 
 configure({ adapter: new Adapter() });
