f6fee77550a04e1a845afcf886d767a3db36f0d1
Thomsen Cummings <thomsencummings@gmail.com>
2021-02-06 14:54:38 -0500

    change server / to * to allow for dynamic frontend routing without failed index.html GETs

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Configure Routes
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
